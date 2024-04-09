1125cbd2e2cadb3eb6bbfd2a85dd387bf4fc9f9c
Rosario Castro Méndez <79597277+ChayoCM0441@users.noreply.github.com>
2024-04-09 07:58:42 -0600

    Add files via upload

---
+++ New_proyect_Eng_Ruth/HTML/script.js
@@ -0,0 +1,15 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	const formData = new FormData(event.target);
+	const username = formData.get('username');
+	const password = formData.get('password');
+
+	console.log(`Username: ${username}, Password: ${password}`);
+});
+
+
+document.getElementById("loginButton").addEventListener("click", function() {
+    window.location.href = "index.html";
+  });
+  
