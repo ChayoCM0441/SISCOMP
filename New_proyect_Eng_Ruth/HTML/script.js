d87c395e3bda9ad5a8ef19cae149e6728f270c25
Manuel <manuelalberto.gorrin@gmail.com>
2024-04-09 11:15:39 -0400

    registers users add

--- New_proyect_Eng_Ruth/HTML/script.js
+++ New_proyect_Eng_Ruth/HTML/script.js
@@ -1,15 +1,55 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-	event.preventDefault();
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
-	const formData = new FormData(event.target);
-	const username = formData.get('username');
-	const password = formData.get('password');
+const app = express();
+const PORT = 3000;
 
-	console.log(`Username: ${username}, Password: ${password}`);
+app.use(express.static(path.join(__dirname)));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
 
-document.getElementById("loginButton").addEventListener("click", function() {
-    window.location.href = "index.html";
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'register.html'));
+});
+
+function saveUsers(usuario) {
+  const usuarios = getUsers();
+  usuarios.push(usuario);
+  fs.writeFileSync('users.json', JSON.stringify(usuarios, null, 2));
+}
+
+function getUsers() {
+  try {
+    const data = fs.readFileSync('users.json', 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+}
+
+app.post('/submit_registration', (req, res) => {
+	const { name, email, password, role } = req.body;
+	saveUsers({ name, email, password, role });
+	res.send('Usuario registrado correctamente.');
   });
-  
+
+app.get('/users', (req, res) => {
+  const usuarios = obtenerUsuarios();
+  res.json(usuarios);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SISCOMP
+
+Como Iniciar el proyecto
+1- Tener nodeJS y npm instalados
+2- Instalar Express comando: npm install express
+3- Ejecutar proyecto node script.js
